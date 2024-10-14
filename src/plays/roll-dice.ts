76393d87a99249032f9f50efe9e7bc6556f8ff39
Matheus Franco <cajondefox@gmail.com>
2024-10-14 04:43:34 -0300

    feat: add mutation tests

--- src/plays/roll-dice.ts
+++ src/plays/roll-dice.ts
@@ -2,6 +2,9 @@ import { Play } from './play';
 
 export class RollDice extends Play {
   public play(sides: number): number {
+    if (sides <= 0) {
+      throw new Error('Side must be greater than 0');
+    }
     return this.randomize(sides) + 1;
   }
 }
--- src/tests/plays/show-letter.test.ts
+++ src/tests/plays/show-letter.test.ts
@@ -1,3 +1,4 @@
+import { ALPHABET } from '../../translations/alphabet.translation';
 import { ShowLetter } from '../../plays/show-letter';
 
 describe(ShowLetter.name, () => {
@@ -10,5 +11,18 @@ describe(ShowLetter.name, () => {
   test('should return a letter', () => {
     const result = adedonha.play();
     expect(result).toMatch(/^[a-zA-Z]+$/);
+  });
+
+  test('should return alphabet size', () => {
+    const size = 26;
+    expect(adedonha.translation[ALPHABET].en).toHaveLength(size);
+  });
+
+  test('should separator be empty string', () => {
+    expect(adedonha.separator).toBe('');
+  });
+
+  test('should letters be the default alphabet', () => {
+    expect(adedonha.letters).toBe(adedonha.translation[ALPHABET].en);
   });
 });
